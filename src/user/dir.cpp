c0b86c56a771c870ee72d2b5e5811f98785d8c71
Petr Kocián <35838356+PetrKocian@users.noreply.github.com>
2021-11-29 20:40:58 +0100

    dir update

--- src/user/dir.cpp
+++ src/user/dir.cpp
@@ -17,7 +17,7 @@ size_t __stdcall dir(const kiv_hal::TRegisters& regs)
 	kiv_os::THandle file_handle = kiv_os::Invalid_Handle;
 	const size_t buffer_size = sizeof(kiv_os::TDir_Entry);
 	char buffer[buffer_size];
-	size_t read = 1;
+	size_t read;
 	size_t written = 0;
 	const char* new_line = "\n";
 	std::string output = "";
@@ -59,7 +59,6 @@ size_t __stdcall dir(const kiv_hal::TRegisters& regs)
 
 	while (!directories.empty())
 	{
-		std::cout << "\nTEST2\n" << directories.at(0).c_str();
 		if (auto result = kiv_os_rtl::Open_File(directories.at(0).c_str(), (kiv_os::NOpen_File)0, kiv_os::NFile_Attributes::Directory, file_handle))
 		{
 			if (strcmp(directories.at(0).c_str(), ".") == 0)
@@ -71,7 +70,6 @@ size_t __stdcall dir(const kiv_hal::TRegisters& regs)
 				output.append("\nDirectory of " + working_dir + directories.at(0).c_str() + "\n\n");
 				output.append("<DIR>\t.\n");
 				output.append("<DIR>\t..\n");
-
 			}
 			read = 1;
 			while (read)
@@ -106,26 +104,21 @@ size_t __stdcall dir(const kiv_hal::TRegisters& regs)
 					}
 					else
 					{
-						std::cout << "\nwrong size of TDir_Entry\n";
+						std::string message = "\nSize not equal to TDir\n";
+						kiv_os_rtl::Write_File(std_out, message.c_str(), message.size(), written);
 						buffer[buffer_size-1] = 0;
 						read = 0;
 					}
-				}
-				else
-				{
-					std::cout << "\nfailed to read\n";
-					read =  0;
 				}
 			}
 		}
 		else
 		{
-			std::cout << "\nfailed to open\n";
+			std::string message = "\nFailed to open directory\n";
+			kiv_os_rtl::Write_File(std_out, message.c_str(), message.size(), written);
 		}
 
-		std::cout << "\nTEST\n";
 		auto it = directories.begin();
-
 		directories.erase(it);
 	}
 
@@ -139,61 +132,4 @@ size_t __stdcall dir(const kiv_hal::TRegisters& regs)
 	kiv_os_rtl::Write_File(std_out, output.data(), output.size(), written);
 
 	return 0;
-}/*
-
-size_t __stdcall dir(const kiv_hal::TRegisters& regs)
-{
-	const kiv_os::THandle std_in = static_cast<kiv_os::THandle>(regs.rax.x);
-	const kiv_os::THandle std_out = static_cast<kiv_os::THandle>(regs.rbx.x);
-
-	const char* file_to_open = reinterpret_cast<const char*>(regs.rdi.r);
-
-	size_t read;
-
-	kiv_os::THandle file_handle = kiv_os::Invalid_Handle;
-
-	char buffer[256];
-
-	if (file_to_open && strlen(file_to_open))
-	{
-		//auto ret_code = kiv_os_rtl::Delete_File(file_to_open);
-
-		uint8_t atr = 0;
-		if (auto ret_code = kiv_os_rtl::Open_File(file_to_open, static_cast<kiv_os::NOpen_File>(atr), kiv_os::NFile_Attributes::System_File, file_handle))
-		{
-			std::cout << "\nOPENED\n";
-		}
-		else
-		{
-			std::cout << "\nOPEN FAILED: " << ret_code << std::endl;
-		}
-		
-	/*	if (auto ret_code = kiv_os_rtl::Write_File(file_handle, "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111112222222222222222222222222222222222222222222222222222222222222222222222222222222222"
-			, strlen("11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111112222222222222222222222222222222222222222222222222222222222222222222222222222222222"), read))
-		{
-			std::cout << "\WRITE\n";
-		}
-		else
-		{
-			std::cout << "\nWRITE FAILED: " << ret_code << std::endl;
-		}
-		
-		if (auto ret_code = kiv_os_rtl::Read_File(file_handle, buffer, 256, read))
-		{
-			std::cout << "\n\nREAD:::: " << read;
-			//for (int i = 0; i < read; i++) {
-			//	printf("%c", buffer[i]);
-			//}
-			std::cout << "file handle: " << file_handle;
-
-			std::cout << buffer <<"\n";
-		}
-		else
-		{
-			std::cout << "\nREAD FAILED: " << ret_code << std::endl;
-		}
-
-
-	}
-	return 0;
-}*/
+}
